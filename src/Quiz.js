145c2d3ba5030204141d24721aa2b0b6fbf3b463
AreejAlmalki <AreejAlmalki1@hotmail.com>
2020-12-26 10:46:29 +0300

    first

---
+++ src/Quiz.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json');
+constructor(props){
+	super(props)
+	]
+class Quiz  extends Component{
+	render(){
+		return (
+		<div>Quiz</div>
+		
+		)
+	}
+}
+
+export default Quiz
